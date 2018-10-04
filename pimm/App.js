f85a7a93566ecfc3e9cb5781f180eae2464a511b
ReidarsOfTheLostArk <reidar.stromme@hotmail.com>
2018-10-04 11:05:26 +0200

    #1 add image to note

--- pimm/App.js
+++ pimm/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import NotePicture from './components/NoteView/NoteMain/NotePicture.js';
 
 
 export default class App extends React.Component {
- 
+	
+	render(){
+		return(
+		<NotePicture/>
+		);
+	}
  
 }
