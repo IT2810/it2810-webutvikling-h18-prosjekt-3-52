b1b25dd3fdb2ea78307eed8074662b0e91d86e26
ReidarsOfTheLostArk <reidar.stromme@hotmail.com>
2018-10-15 12:55:18 +0200

    one more test

---
+++ pimm/components/NoteView/__tests__/NoteTopBar-test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'react-native';
+import NoteTopBar from '../NoteTopBar.js';
+import renderer from 'react-test-renderer';
+
+test('NoteTopBar renders correctly', ()=>{
+	const ntb = renderer.create(<NoteTopBar />).toJSON();
+    expect(ntb).toMatchSnapshot();
+    expect(ntb.type).toBe("View");
+	expect(ntb.children[0].type).toBe("View");
+	expect(ntb.children[1].type).toBe("View");
+});
