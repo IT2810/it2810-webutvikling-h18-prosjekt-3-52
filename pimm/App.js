51df054edd029278468c8753ce649eaa8939551b
matiasre <matiasreidem@gmail.com>
2018-10-17 05:44:41 +0200

    #22 Make indentation and code style consistent

--- pimm/App.js
+++ pimm/App.js
@@ -12,65 +12,78 @@ import { AsyncStorage, Button } from 'react-native';
 
 
 export default class App extends React.Component {
-	constructor(props){
-		super(props);
-		this.state={
-			title: "",
-			note: "",
-			image: "",
-			view: true,
-			fromLoaded: false,
-			oldTitle: "",
-		}
-	}
-	
-	changeView(){
-		this.setState({view: !this.state.view});
-	}
-	
-	getTitle(newTitle){
-		this.setState({title: newTitle});
-	}
-	getNote(newNote){
-		this.setState({note: newNote});
-		
-	}
-	getImage(newImage){
-		this.setState({image: newImage});
-		
-	}
-	
-	fromLoadedF(){
-		this.setState({fromLoaded: false});
-	}
-	
-	fromLoadedT(){
-		this.setState({fromLoaded: true});
-	}
-	
-	changeOldTitle(title){
-		this.setState({oldTitle: title});
-	}
-	
-	
-	
-	
-	render(){
-		
-		if(this.state.view===true){
-		return(
-			<ScrollView style={{paddingTop: 22}}>
-			<StartView changeView={this.changeView.bind(this)} sendTitleUp={this.getTitle.bind(this)} sendNoteUp={this.getNote.bind(this)} 
-			sendImageUp={this.getImage.bind(this)} sfl={this.fromLoadedT.bind(this)} changeOldTitle={this.changeOldTitle.bind(this)}/>
-			</ScrollView>);
-		}else{
-			return(
-			<ScrollView style={{paddingTop: 22}}>
-			<NoteView oldTitle={this.state.oldTitle} changeView={this.changeView.bind(this)} sendTitleUp={this.getTitle.bind(this)} sendNoteUp={this.getNote.bind(this)}
-			sendImageUp={this.getImage.bind(this)} title={this.state.title} note={this.state.note} image={this.state.image} fl={this.state.fromLoaded} sfl={this.fromLoadedF.bind(this)}/>
-			</ScrollView>
-			);
-		}
-	}
+    constructor(props){
+        super(props);
+        this.state={
+            title: "",
+            note: "",
+            image: "",
+            view: true,
+            fromLoaded: false,
+            oldTitle: "",
+        }
+    }
+
+    changeView(){
+        this.setState({view: !this.state.view});
+    }
+
+    getTitle(newTitle){
+        this.setState({title: newTitle});
+    }
+    getNote(newNote){
+        this.setState({note: newNote});
+
+    }
+    getImage(newImage){
+        this.setState({image: newImage});
+
+    }
+
+    fromLoadedF(){
+        this.setState({fromLoaded: false});
+    }
+
+    fromLoadedT(){
+        this.setState({fromLoaded: true});
+    }
+
+    changeOldTitle(title){
+        this.setState({oldTitle: title});
+    }
+
+
+
+
+    render(){
+
+        if(this.state.view===true){
+            return(
+                <ScrollView style={{paddingTop: 22}}>
+                    <StartView
+                        changeView={this.changeView.bind(this)}
+                        sendTitleUp={this.getTitle.bind(this)}
+                        sendNoteUp={this.getNote.bind(this)}
+                        sendImageUp={this.getImage.bind(this)}
+                        sfl={this.fromLoadedT.bind(this)}
+                        changeOldTitle={this.changeOldTitle.bind(this)}/>
+                </ScrollView>
+            );
+        }else{
+            return(
+                <ScrollView style={{paddingTop: 22}}>
+                    <NoteView
+                        oldTitle={this.state.oldTitle}
+                        changeView={this.changeView.bind(this)}
+                        sendTitleUp={this.getTitle.bind(this)}
+                        sendNoteUp={this.getNote.bind(this)}
+                        sendImageUp={this.getImage.bind(this)}
+                        title={this.state.title} note={this.state.note}
+                        image={this.state.image} fl={this.state.fromLoaded}
+                        sfl={this.fromLoadedF.bind(this)}/>
+                </ScrollView>
+            );
+        }
+    }
 
 }
--- pimm/components/NoteView/NoteTopBar/SaveButton.js
+++ pimm/components/NoteView/NoteTopBar/SaveButton.js
@@ -2,39 +2,41 @@ import React from 'react';
 import { AsyncStorage, Text, View, Button, Alert } from 'react-native';
 import {NotePicture} from '../NoteMain/NotePicture.js';
 import Title from '../NoteMain/Title.js';
-import {NoteText} from '../NoteMain/NoteText.js';
+import { NoteText } from '../NoteMain/NoteText.js';
 
 export default class SaveButton extends React.Component{
-  render(){
-      return (
-		<View style={{width: 100}}>
-        <Button
-          onPress={this.saveNote}
-          title="Save note"
-		  color="#B8860B"
-        />
-		</View>);
-  }
-  
-  	saveNote = async () => {
-		if(this.props.title==""){
-			alert("Please enter a title");
-			return;
-		}
-		
-		let obj={
-			title: this.props.title,
-			note: this.props.note,
-			image: this.props.image
-		}
-		try {
-			await AsyncStorage.removeItem(this.props.oldTitle);
-			await AsyncStorage.setItem(this.props.title, JSON.stringify(obj));
-		} catch (error) {
-			alert("Couldn't save")
-		}
-	}
-  
+    render(){
+        return (
+            <View style={{width: 100}}>
 
+                <Button
+                    onPress={this.saveNote}
+                    title="Save note"
+                    color="#B8860B"
+                />
 
+            </View>
+        );
+    }
+
+    saveNote = async () => {
+
+        if(this.props.title === ""){
+            alert("Please enter a title");
+            return;
+        }
+
+        let obj={
+            title: this.props.title,
+            note: this.props.note,
+            image: this.props.image
+        };
+        try {
+            await AsyncStorage.removeItem(this.props.oldTitle);
+            await AsyncStorage.setItem(this.props.title, JSON.stringify(obj));
+
+        } catch (error) {
+            alert("Couldn't save")
+        }
+    }
 }
