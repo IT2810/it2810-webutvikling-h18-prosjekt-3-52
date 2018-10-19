1336273334493a2173e944ddc1b29a1c0f6b2398
matiasre <matiasreidem@gmail.com>
2018-10-19 18:47:13 +0200

    Made NotePicture not try to make an Image with null as source. Closes #26

--- pimm/components/NoteView/NoteMain/NotePicture.js
+++ pimm/components/NoteView/NoteMain/NotePicture.js
@@ -11,19 +11,38 @@ export default class NotePicture extends React.Component{
     render() {
         let { image } = this.state;
 
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Button
-                    title="Pick an image that fits to your note"
-                    onPress={this.pickImage}
-                    color="#B8860B"
-                />
 
-                <View style={{ borderColor: 'black', borderWidth: 1, backgroundColor: 'beige', width: 280, height: 300 }}>
-                    <Image source={{ uri: image }} style={{ width: 280, height: 300 }} />
+        if (image === null) {
+            return (
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Button
+                        title="Pick an image that fits to your note"
+                        onPress={this.pickImage}
+                        color="#B8860B"
+                    />
+
+                    <View style={{ borderColor: 'black', borderWidth: 1, backgroundColor: 'beige', width: 280, height: 300 }}>
+                        
+                    </View>
                 </View>
-            </View>
-        );
+            );
+        } else {
+            return (
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Button
+                        title="Pick an image that fits to your note"
+                        onPress={this.pickImage}
+                        color="#B8860B"
+                    />
+
+                    <View style={{ borderColor: 'black', borderWidth: 1, backgroundColor: 'beige', width: 280, height: 300 }}>
+                        <Image source={{ uri: image }} style={{ width: 280, height: 300 }} />
+                    </View>
+                </View>
+            );
+        }
+
+
     }
 
     componentWillReceiveProps(nextProps){
